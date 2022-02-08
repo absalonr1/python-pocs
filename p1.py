339c79b39f013bc7671d4268df4fa9a5bce0bce2
Absalon <luisopazo@gmail.com>
2022-02-08 14:48:06 -0300

    init

--- p1.py
+++ p1.py
@@ -1,7 +1,22 @@
 import subprocess
 import re
+# python3 -m pip install requests
+import requests
+#import json
 
-routesFileName="routes-sub.json"
+# r = requests.get('http://adminkong.dev.blue.private/services/5d197700-18a4-43a6-b095-b4535d282dc6/plugins')
+
+# jsonRespDictionary = r.json()
+# print(type(jsonRespDictionary))
+
+# for item in jsonRespDictionary["data"]:
+#     print(item["name"])
+
+
+# if(True):
+#     quit()
+
+routesFileName="routes.json"
 servicesFileName="services.json"
 
 subPrc = subprocess.Popen(['jq','(.data[].paths[])',routesFileName], stdout = subprocess.PIPE) 
@@ -17,6 +32,7 @@ csvSeparator = ","
 
 myFile.write("path"+csvSeparator)
 myFile.write("svcId"+csvSeparator)
+myFile.write("svcPlugins"+csvSeparator)
 myFile.write("albDNS"+csvSeparator)
 myFile.write("svcName"+csvSeparator)
 myFile.write("k8sSvcName"+csvSeparator)
@@ -26,6 +42,7 @@ myFile.write("k8sDeployName\n")
 
 path=""
 svcId=""
+svcPlugins=""
 albDNS=""
 svcName=""
 k8sSvcName=""
@@ -45,87 +62,102 @@ for path in pathArray:
         if(len(svcId)>38):
             svcId="ERROR: Mas de un service para el path"
         else:
-            subPrc = subprocess.Popen(['jq','(.data[] | select (.id == '+svcId+' ) )',servicesFileName], stdout = subprocess.PIPE) 
-            subPrc2 = subprocess.Popen(['jq','.host'], stdin=subPrc.stdout, stdout = subprocess.PIPE)
-            albDNS = str(subPrc2.communicate()[0].decode("utf-8"))
-            albDNS = albDNS.lstrip().rstrip()
-            print("ALB DNS: "+ albDNS)
+            # http://adminkong.dev.blue.private/services/5d197700-18a4-43a6-b095-b4535d282dc6/plugins
+
+            # Identificar los plugins asociados al service
+            req = requests.get('http://adminkong.dev.blue.private/services/'+svcId[1:len(svcId)-1]+"/plugins")
+            jsonRespDictionary = req.json()
+            flagLambda = False
+            #print(jsonRespDictionary)
+            if(len(jsonRespDictionary)>0):
+                for item in jsonRespDictionary["data"]:
+                    svcPlugins= svcPlugins+";"+item["name"]
+                    if(svcPlugins.find("lambda") != -1):
+                        flagLambda = True
             
-            
-            if(re.match("[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+",albDNS[1:len(albDNS)-1]) is None ):
+            if(not flagLambda):
                 subPrc = subprocess.Popen(['jq','(.data[] | select (.id == '+svcId+' ) )',servicesFileName], stdout = subprocess.PIPE) 
-                subPrc2 = subprocess.Popen(['jq','.name'], stdin=subPrc.stdout, stdout = subprocess.PIPE) 
-                svcName = str(subPrc2.communicate()[0].decode("utf-8"))
-                svcName = svcName.lstrip().rstrip()
-                print("Service NAME: "+ svcName)
-
-                # quitar comillas dobles al inicio y al final
-                path = path[1:len(path)-1]
-
-                path1=""
-                path2=""
-                path3=""
-                path4=""
-
-                if(path.endswith("/")):
-                    path1=path
-                    path2=path+"*"
-                    path3=path[0:len(path)-1]+"*"
-                    path4=path[0:len(path)-1]
-                else:
-                    path1=path
-                    path2=path+"*"
-                    path3=path+"/*"
-                    path4=path+"/"
-                print("\t"+path1)
-                print("\t"+path2)
-                print("\t"+path3)
-                print("\t"+path4)
+                subPrc2 = subprocess.Popen(['jq','.host'], stdin=subPrc.stdout, stdout = subprocess.PIPE)
+                albDNS = str(subPrc2.communicate()[0].decode("utf-8"))
+                albDNS = albDNS.lstrip().rstrip()
+                print("ALB DNS: "+ albDNS)
                 
-                #jq '.items[] | select ( .status.loadBalancer.ingress[].hostname == "internal-k8s-ingresspyme2c-deffada9e5-1182658737.us-west-2.elb.amazonaws.com" and ( .spec.rules[].http.paths[].path == "/api/pyme2c/compensations/v1" or .spec.rules[].http.paths[].path == "/api/pyme2c/compensations/v1/" or .spec.rules[].http.paths[].path == "/api/pyme2c/compensations/v1/*" or .spec.rules[].http.paths[].path == "/api/pyme2c/compensations/v1*")) ' ingress.json | jq '.spec.rules[].http.paths[0].backend.service.name'
                 
-                subPrc = subprocess.Popen(['jq','.items[] | select ( .status.loadBalancer.ingress[].hostname == '+albDNS+' and ( .spec.rules[].http.paths[].path == "'+path1+'" or .spec.rules[].http.paths[].path == "'+path2+'" or .spec.rules[].http.paths[].path == "'+path3+'" or .spec.rules[].http.paths[].path == "'+path4+'"))','ingress.json'], stdout = subprocess.PIPE) 
-                subPrc2 = subprocess.Popen(['jq','.spec.rules[].http.paths[0].backend.service.name'], stdin=subPrc.stdout, stdout = subprocess.PIPE) 
-                k8sSvcName = str(subPrc2.communicate()[0].decode("utf-8"))
-                k8sSvcName = k8sSvcName.lstrip().rstrip()
-                k8sSvcName= k8sSvcName[1:len(k8sSvcName)-1]
-                print("K8S-Service NAME: "+ k8sSvcName)
+                if(re.match("[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+",albDNS[1:len(albDNS)-1]) is None ):
+                    subPrc = subprocess.Popen(['jq','(.data[] | select (.id == '+svcId+' ) )',servicesFileName], stdout = subprocess.PIPE) 
+                    subPrc2 = subprocess.Popen(['jq','.name'], stdin=subPrc.stdout, stdout = subprocess.PIPE) 
+                    svcName = str(subPrc2.communicate()[0].decode("utf-8"))
+                    svcName = svcName.lstrip().rstrip()
+                    print("Service NAME: "+ svcName)
 
-                subPrc = subprocess.Popen(['jq','.items[] | select ( .status.loadBalancer.ingress[].hostname == '+albDNS+' and ( .spec.rules[].http.paths[].path == "'+path1+'" or .spec.rules[].http.paths[].path == "'+path2+'" or .spec.rules[].http.paths[].path == "'+path3+'" or .spec.rules[].http.paths[].path == "'+path4+'"))','ingress.json'], stdout = subprocess.PIPE) 
-                subPrc2 = subprocess.Popen(['jq','.metadata.namespace'], stdin=subPrc.stdout, stdout = subprocess.PIPE) 
-                k8sNameSpace = str(subPrc2.communicate()[0].decode("utf-8"))
-                k8sNameSpace = k8sNameSpace.lstrip().rstrip()
-                k8sNameSpace= k8sNameSpace[1:len(k8sNameSpace)-1]
-                print("K8S-Namespace: "+ k8sNameSpace)
+                    # quitar comillas dobles al inicio y al final
+                    path = path[1:len(path)-1]
 
-                # Route Path: "/api/container/core/v1"
-                # Service ID: "8377c5a7-d145-40c2-a48f-afb0f90d8038"
-                # ALB DNS: "internal-k8s-ingresscontainer-fcc9610877-445198957.us-west-2.elb.amazonaws.com"
-                # Service NAME: "dev-srv00019-cross-front-contain"
-                # K8S-Service NAME: "ms-container-core"
-                # K8S-Namespace: "dev-ns-containers"
+                    path1=""
+                    path2=""
+                    path3=""
+                    path4=""
 
-                # obtener selector app de k8s para el svc
-                # k get svc ms-salida-ruta -n dev-ns-retiro -o json | jq '.spec.selector.app'
+                    if(path.endswith("/")):
+                        path1=path
+                        path2=path+"*"
+                        path3=path[0:len(path)-1]+"*"
+                        path4=path[0:len(path)-1]
+                    else:
+                        path1=path
+                        path2=path+"*"
+                        path3=path+"/*"
+                        path4=path+"/"
+                    print("\t"+path1)
+                    print("\t"+path2)
+                    print("\t"+path3)
+                    print("\t"+path4)
+                    
+                    #jq '.items[] | select ( .status.loadBalancer.ingress[].hostname == "internal-k8s-ingresspyme2c-deffada9e5-1182658737.us-west-2.elb.amazonaws.com" and ( .spec.rules[].http.paths[].path == "/api/pyme2c/compensations/v1" or .spec.rules[].http.paths[].path == "/api/pyme2c/compensations/v1/" or .spec.rules[].http.paths[].path == "/api/pyme2c/compensations/v1/*" or .spec.rules[].http.paths[].path == "/api/pyme2c/compensations/v1*")) ' ingress.json | jq '.spec.rules[].http.paths[0].backend.service.name'
+                    
+                    subPrc = subprocess.Popen(['jq','.items[] | select ( .status.loadBalancer.ingress[].hostname == '+albDNS+' and ( .spec.rules[].http.paths[].path == "'+path1+'" or .spec.rules[].http.paths[].path == "'+path2+'" or .spec.rules[].http.paths[].path == "'+path3+'" or .spec.rules[].http.paths[].path == "'+path4+'"))','ingress.json'], stdout = subprocess.PIPE) 
+                    subPrc2 = subprocess.Popen(['jq','.spec.rules[].http.paths[0].backend.service.name'], stdin=subPrc.stdout, stdout = subprocess.PIPE) 
+                    k8sSvcName = str(subPrc2.communicate()[0].decode("utf-8"))
+                    k8sSvcName = k8sSvcName.lstrip().rstrip()
+                    k8sSvcName= k8sSvcName[1:len(k8sSvcName)-1]
+                    print("K8S-Service NAME: "+ k8sSvcName)
 
-                subPrc = subprocess.Popen(['kubectl get svc '+k8sSvcName+' -n '+k8sNameSpace+' -o json'], shell=True, stdout = subprocess.PIPE) 
-                subPrc2 = subprocess.Popen(['jq','.spec.selector.app'], stdin=subPrc.stdout, stdout = subprocess.PIPE)  
-                selectorApp = str(subPrc2.communicate()[0].decode("utf-8"))
-                selectorApp = selectorApp.lstrip().rstrip()
-                print("K8S-selectorApp: "+ selectorApp) 
-                
-                print("command: "+'kubectl get deploy -n '+k8sNameSpace+' -o json |'+ 'jq .items[] | select(.spec.template.metadata.labels.app == '+selectorApp+' ) | jq .metadata.name')
-                subPrc = subprocess.Popen(['kubectl get deploy -n '+k8sNameSpace+' -o json'], shell=True, stdout = subprocess.PIPE) 
-                subPrc2 = subprocess.Popen(['jq','.items[] | select(.spec.template.metadata.labels.app == '+selectorApp+' )'], stdin=subPrc.stdout, stdout = subprocess.PIPE)  
-                subPrc3 = subprocess.Popen(['jq','.metadata.name'], stdin=subPrc2.stdout, stdout = subprocess.PIPE)  
-                k8sDeployName = str(subPrc3.communicate()[0].decode("utf-8"))
-                k8sDeployName = k8sDeployName.lstrip().rstrip()
-                print("K8S-deployName: "+ k8sDeployName)
+                    subPrc = subprocess.Popen(['jq','.items[] | select ( .status.loadBalancer.ingress[].hostname == '+albDNS+' and ( .spec.rules[].http.paths[].path == "'+path1+'" or .spec.rules[].http.paths[].path == "'+path2+'" or .spec.rules[].http.paths[].path == "'+path3+'" or .spec.rules[].http.paths[].path == "'+path4+'"))','ingress.json'], stdout = subprocess.PIPE) 
+                    subPrc2 = subprocess.Popen(['jq','.metadata.namespace'], stdin=subPrc.stdout, stdout = subprocess.PIPE) 
+                    k8sNameSpace = str(subPrc2.communicate()[0].decode("utf-8"))
+                    k8sNameSpace = k8sNameSpace.lstrip().rstrip()
+                    k8sNameSpace= k8sNameSpace[1:len(k8sNameSpace)-1]
+                    print("K8S-Namespace: "+ k8sNameSpace)
 
-                print("------")
+                    # Route Path: "/api/container/core/v1"
+                    # Service ID: "8377c5a7-d145-40c2-a48f-afb0f90d8038"
+                    # ALB DNS: "internal-k8s-ingresscontainer-fcc9610877-445198957.us-west-2.elb.amazonaws.com"
+                    # Service NAME: "dev-srv00019-cross-front-contain"
+                    # K8S-Service NAME: "ms-container-core"
+                    # K8S-Namespace: "dev-ns-containers"
+
+                    # obtener selector app de k8s para el svc
+                    # k get svc ms-salida-ruta -n dev-ns-retiro -o json | jq '.spec.selector.app'
+
+                    subPrc = subprocess.Popen(['kubectl get svc '+k8sSvcName+' -n '+k8sNameSpace+' -o json'], shell=True, stdout = subprocess.PIPE) 
+                    subPrc2 = subprocess.Popen(['jq','.spec.selector.app'], stdin=subPrc.stdout, stdout = subprocess.PIPE)  
+                    selectorApp = str(subPrc2.communicate()[0].decode("utf-8"))
+                    selectorApp = selectorApp.lstrip().rstrip()
+                    print("K8S-selectorApp: "+ selectorApp) 
+                    
+                    print("command: "+'kubectl get deploy -n '+k8sNameSpace+' -o json |'+ 'jq .items[] | select(.spec.template.metadata.labels.app == '+selectorApp+' ) | jq .metadata.name')
+                    subPrc = subprocess.Popen(['kubectl get deploy -n '+k8sNameSpace+' -o json'], shell=True, stdout = subprocess.PIPE) 
+                    subPrc2 = subprocess.Popen(['jq','.items[] | select(.spec.template.metadata.labels.app == '+selectorApp+' )'], stdin=subPrc.stdout, stdout = subprocess.PIPE)  
+                    subPrc3 = subprocess.Popen(['jq','.metadata.name'], stdin=subPrc2.stdout, stdout = subprocess.PIPE)  
+                    k8sDeployName = str(subPrc3.communicate()[0].decode("utf-8"))
+                    k8sDeployName = k8sDeployName.lstrip().rstrip()
+                    print("K8S-deployName: "+ k8sDeployName)
+
+                    print("------")
         
         myFile.write(path+csvSeparator)
         myFile.write(svcId+csvSeparator)
+        myFile.write(svcPlugins+csvSeparator)
         myFile.write(albDNS+csvSeparator)
         myFile.write(svcName+csvSeparator)
         myFile.write(k8sSvcName+csvSeparator)
@@ -136,6 +168,7 @@ for path in pathArray:
 
         path=""
         svcId=""
+        svcPlugins=""
         albDNS=""
         svcName=""
         k8sSvcName=""
